d0cc84d5f4e07a7f50fd84bea47705aeb27ec09c
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-07-10 13:47:24 +0800

    tailwindcss / daisyui / icons setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import { defineNuxtConfig } from "nuxt/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -5,7 +6,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon"],
-  css: ["~assets/css/main.css"],
+  css: ["assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
